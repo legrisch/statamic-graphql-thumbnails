1f251ae8d3285c3d2aede6cc5d53279278b4b48d
Grischa Erbe <grischa.erbe@googlemail.com>
2021-04-24 21:30:36 +0200

    use fit parameter

--- src/ThumbnailsServiceProvider.php
+++ src/ThumbnailsServiceProvider.php
@@ -22,25 +22,40 @@ class ThumbnailsServiceProvider extends AddonServiceProvider
                     ],
                     'height' => [
                         'type' => GraphQL::int(),
+                    ],
+                    'fit' => [
+                        'type' => GraphQL::string(),
                     ]
                 ],
-                'resolve' => function ($entry, $args) {
+                'resolve' => function ($asset, $args) {
                     try {
                         $width = $args["width"] ?? null;
                         $height = $args["height"] ?? null;
+                        $fit = $args["fit"] ?? null;
 
                         if ($width === null && $height === null) {
-                            throw new \Exception("Error Processing Request: Please provide a width or a height", 1);
+                            throw new \Exception("Please provide a width and/or a height", 1);
                         }
 
-                        if ($entry == null || !$entry->isImage()) return null;
+                        if ($asset === null || !$asset->isImage()) return null;
 
-                        $image = Image::manipulate($entry);
+                        $image = Image::manipulate($asset);
+
                         if ($width) {
                             $image = $image->width($width);
                         }
                         if ($height) {
                             $image = $image->height($height);
+                        }
+
+                        $allowedFitParams = ["contain", "max", "fill", "stretch", "crop", "crop_focal"];
+                        if ($fit !== null && !in_array($fit, $allowedFitParams)) {
+                            throw new \Exception("Provided fit not found, refer to the docs for possible values: https://statamic.dev/tags/glide#parameters", 1);
+                        }
+                        if ($fit) {
+                            $image->fit($fit);
+                        } else {
+                            $image->fit("crop_focal");
                         }
                         $url = $image->build();
                         return URL::makeAbsolute($url);
